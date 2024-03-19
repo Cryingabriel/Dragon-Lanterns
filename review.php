eefe0f899156b6174a5130854f6ee67343465415
Cryingabriel <Cai19mac@gmail.com>
2024-03-19 15:48:09 -0600

    website review

---
+++ review.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["Name"]; ?><br>
+Your email address is: <?php echo $_POST["Email"]; ?>
+your review says: <?php echo $_POST["Message"]; ?>
+
+</body>
+</html>
